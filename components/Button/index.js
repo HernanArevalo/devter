1990d6d002a81338b4f4655e202571979905b892
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-01-24 20:58:01 -0300

    Home style

---
+++ components/Button/index.js
@@ -0,0 +1,33 @@
+import { colors } from "../../styles/theme";
+
+export default function Button ( { children, onClick } ) {
+
+    return (
+        <>
+            <button onClick={ onClick }>
+                {children}
+            </button>
+
+            <style jsx>{`
+                button{
+                    align-items: center;
+                    background: ${ colors.black };
+                    border-radius: 9999px;
+                    border: 0;
+                    color: #fff;
+                    cursor: pointer;
+                    display: flex;
+                    font-weight: 800;
+                    gap: 10px;
+                    padding: 10px 24px;
+                    transition: opacity .3s ease;
+                }
+                button:hover{
+                    opacity: .7
+                }
+            `}</style>
+        </>
+    )
+
+
+}
---
+++ styles/theme.js
@@ -0,0 +1,15 @@
+
+export const breakpoints = {
+    mobile: '500px'
+}
+
+export const fonts = {
+    base: 'Helvetica Neue, sans-serif'
+}
+
+export const colors = {
+    primary: '#0099ff',
+    secondary: '#1c5480',
+    black: '#000000',
+    white: '#ffffff'
+}
