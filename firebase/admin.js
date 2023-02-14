e47a18079057b995664da150b4d44380243e573c
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-02-14 18:02:56 -0300

    .env file

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-keys.json");
+var serviceAccount = require(process.env.FIREBASE_KEYS);
 
 try{
 admin.initializeApp({
